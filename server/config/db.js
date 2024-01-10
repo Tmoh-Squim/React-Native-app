245793417f4942db8730a47d015d115ed0eb3063
Timoth Kibunja <122232330+Tmoh-Squim@users.noreply.github.com>
2024-01-10 10:39:57 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 exports.connectDB = async ()=>{
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect('mongodb+srv://timoohwilliams885:y4ly6aqd9zmikfHd@cluster1.3uhu857.mongodb.net/')
     .then(console.log('mongodb connected successfully'))
     .catch((err)=>{
         console.log(err)
