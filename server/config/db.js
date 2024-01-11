cdf175084b6d9b3dfe09ce156bb21dbd56072b68
Timoth Kibunja <122232330+Tmoh-Squim@users.noreply.github.com>
2024-01-11 08:00:03 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 exports.connectDB = async ()=>{
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URL,{
+        useUnifiedTopology:true)
     .then(console.log('mongodb connected successfully'))
     .catch((err)=>{
         console.log(err)
