33dd9cc01cceb3b85403b47c6bcf1e78cb522847
Timoth Kibunja <122232330+Tmoh-Squim@users.noreply.github.com>
2024-01-11 08:14:37 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose')
 exports.connectDB = async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL)
-        .then(console.log('mongodb connected successfully'))
-        .catch((err)=>{
-            console.log(err)
-        })
+        console.log('mongodb connected successfully')
+        
     } catch (error) {
         console.log('error',error)
     }
